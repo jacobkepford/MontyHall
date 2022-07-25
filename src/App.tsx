edbcd398119cdbdca2b346bdb21ef1a6977887e0
Jacob Kepford <rtbredge@gmail.com>
2022-07-24 20:23:00 -0500

    updated app page to be a form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div className="form-group mb-3">
+          <label htmlFor="numberOfBoxes">How many boxes?</label>
+          <input type="number" className="form-control" id="numberOfBoxes" />
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="numberOfRuns">
+            How many times should the program run?
+          </label>
+          <input type="number" className="form-control" id="numberOfRuns" />
+        </div>
+        <button type="button" className="btn btn-primary">
+          Run Monty Hall Problem
+        </button>
+      </form>
     </div>
   );
 }
