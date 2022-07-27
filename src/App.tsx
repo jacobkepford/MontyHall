36dd3290bef0cd13f919b146745c3a0fa89170f0
Jacob Kepford <rtbredge@gmail.com>
2022-07-26 21:06:18 -0500

    moved main form to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MainForm from "./components/MainForm";
 
 function App() {
   return (
     <div className="App">
-      <form>
-        <div className="form-group mb-3">
-          <label htmlFor="numberOfBoxes">How many boxes?</label>
-          <input
-            type="number"
-            min={3}
-            max={10}
-            className="form-control"
-            id="numberOfBoxes"
-          />
-        </div>
-        <div className="form-group mb-3">
-          <label htmlFor="numberOfRuns">
-            How many times should the program run?
-          </label>
-          <input type="number" className="form-control" id="numberOfRuns" />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Run Monty Hall Problem
-        </button>
-      </form>
+      <MainForm />
     </div>
   );
 }
---
+++ src/components/MainForm.tsx
@@ -0,0 +1,27 @@
+function MainForm() {
+  return (
+    <form>
+      <div className="form-group mb-3">
+        <label htmlFor="numberOfBoxes">How many boxes?</label>
+        <input
+          type="number"
+          min={3}
+          max={10}
+          className="form-control"
+          id="numberOfBoxes"
+        />
+      </div>
+      <div className="form-group mb-3">
+        <label htmlFor="numberOfRuns">
+          How many times should the program run?
+        </label>
+        <input type="number" className="form-control" id="numberOfRuns" />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Run Monty Hall Problem
+      </button>
+    </form>
+  );
+}
+
+export default MainForm;
