f78c189dfabb76ec1e91f9ee8a5f271837d131bf
Jacob Kepford <rtbredge@gmail.com>
2022-07-26 21:46:52 -0500

    Created GameCounters component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MainForm from "./components/MainForm";
+import GameCounters from "./components/GameCounters";
 
 function App() {
   return (
     <div className="App">
       <MainForm />
+      <GameCounters />
     </div>
   );
 }
---
+++ src/components/GameCounters.tsx
@@ -0,0 +1,20 @@
+function GameCounters() {
+  return (
+    <div className="row mt-5 align-items-center text-center">
+      <div className="col-sm">
+        <h1>0</h1>
+        <h1>Program Counter</h1>
+      </div>
+      <div className="col-sm">
+        <h1>0</h1>
+        <h1>Win Counter</h1>
+      </div>
+      <div className="col-sm">
+        <h1>0%</h1>
+        <h1>Win Percentage</h1>
+      </div>
+    </div>
+  );
+}
+
+export default GameCounters;
