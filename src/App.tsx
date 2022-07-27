100944b1882d14440bf9eeaf97dd0f5ee5f66d0a
Jacob Kepford <rtbredge@gmail.com>
2022-07-26 22:15:08 -0500

    added props to Main Form

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MainForm from "./components/MainForm";
 import GameCounters from "./components/GameCounters";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [programRuns, setProgramRuns] = useState(0);
+  const [boxCount, setBoxCount] = useState(0);
+
+  const setRuns = (runCount: number) => setProgramRuns(runCount);
+  const setBoxes = (boxTotal: number) => setBoxCount(boxTotal);
+
   return (
     <div className="App">
-      <MainForm />
+      <MainForm setRuns={setRuns} setBoxes={setBoxes} />
       <GameCounters />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -1,6 +1,20 @@
-function MainForm() {
+import { useState } from "react";
+
+type MainFormProps = {
+  setRuns: Function;
+  setBoxes: Function;
+};
+
+const HandleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+};
+
+const MainForm = (props: MainFormProps) => {
+  const [boxCount, setBoxCount] = useState(0);
+  const [runCount, setRunCount] = useState(0);
+
   return (
-    <form>
+    <form onSubmit={HandleSubmit}>
       <div className="form-group mb-3">
         <label htmlFor="numberOfBoxes">How many boxes?</label>
         <input
@@ -9,19 +23,25 @@ function MainForm() {
           max={10}
           className="form-control"
           id="numberOfBoxes"
+          onChange={(event) => setBoxCount(parseInt(event.target.value))}
         />
       </div>
       <div className="form-group mb-3">
         <label htmlFor="numberOfRuns">
           How many times should the program run?
         </label>
-        <input type="number" className="form-control" id="numberOfRuns" />
+        <input
+          type="number"
+          className="form-control"
+          id="numberOfRuns"
+          onChange={(event) => setRunCount(parseInt(event.target.value))}
+        />
       </div>
       <button type="button" className="btn btn-primary">
         Run Monty Hall Problem
       </button>
     </form>
   );
-}
+};
 
 export default MainForm;
